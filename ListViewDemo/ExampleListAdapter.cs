84753783c18a4ce51713de00c5b22612a350b37f
Elvis Xia <xux@microsoft.com>
2017-11-17 15:37:44 +0800

    Add project files.

---
+++ ListViewDemo/ExampleListAdapter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace ListViewDemo
+{
+    public class ExampleListAdapter : BaseAdapter<Item>
+    {
+        private Activity mContext;
+        private List<Item> mList;
+
+        public ExampleListAdapter(Activity context, List<Item> list)
+        {
+            mContext = context;
+            mList = list;
+        }
+
+        public override Item this[int position] => mList[position];
+
+        public override int Count => mList.Count;
+
+        public override long GetItemId(int position)
+        {
+            return mList[position].Id;
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            var item = mList[position];
+
+            if (convertView == null)
+            {
+                convertView = mContext.LayoutInflater.Inflate(Resource.Layout.CustomRowView,null);
+            }
+
+            convertView.FindViewById<TextView>(Resource.Id.mText).Text = item.Text;
+            return convertView;
+        }
+    }
+}
---
+++ ListViewDemo/MainActivity.cs
@@ -0,0 +1,58 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System.Collections.Generic;
+
+namespace ListViewDemo
+{
+    [Activity(Label = "ListViewDemo", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        ListView exampleListView;
+        
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            exampleListView = FindViewById<ListView>(Resource.Id.exampleListView);
+
+            var items = InitList();
+
+            exampleListView.Adapter = new ExampleListAdapter(this, items);
+
+            exampleListView.ItemClick += ExampleListView_ItemClick;
+        }
+
+        private void ExampleListView_ItemClick(object sender, AdapterView.ItemClickEventArgs e)
+        {
+            //the following codes are triggered correctly
+            var abc = 123;
+        }
+
+        private List<Item> InitList()
+        {
+            List<Item> items = new List<Item>();
+            for (int i = 0; i < 10; i++)
+            {
+                items.Add(new Item(i, "Item " + i));
+            }
+            return items;
+        }
+    }
+
+
+    public class Item
+    {
+        public Item(int id, string text)
+        {
+            this.Id = id;
+            this.Text = text;
+        }
+        public int Id { get; set; }
+        public string Text { get; set; }
+    }
+}
+
